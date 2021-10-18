d39f02410fdab520cdc444c0eaba2d77506f5d4e
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-10-19 03:16:09 +0500

    Update

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,6 +1,6 @@
 class Configuration {
-  // apiBaseUrl = "https://screening-app-backend-bn654.ondigitalocean.app/";
-  apiBaseUrl = "http://localhost:8080/";
+  apiBaseUrl = "https://screening-app-backend-bn654.ondigitalocean.app/";
+//   apiBaseUrl = "http://localhost:8080/";
 }
 
 export default Configuration;
