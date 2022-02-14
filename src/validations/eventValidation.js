100d61254b93b5c088d31359dc09a662b3297975
Syed Sarosh Farrukh <sarosh@dynamiconlinetechnologies.com>
2022-02-14 18:26:44 +0500

    Date Validation Fix

--- src/validations/eventValidation.js
+++ src/validations/eventValidation.js
@@ -7,7 +7,7 @@ class EventValidation {
       title: Yup.string().required("Required!").min(2).max(100),
       description: Yup.string().required("Required!").min(10).max(10000),
       startingDate : Yup.date().required("Required!"),
-      endingDate : Yup.date().required("Required!"),
+      endingDate : Yup.date().min(Yup.ref('startingDate'), 'Ending date must be greater than starting date').required("Required!"),
       // image : Yup.mixed()
       //   .required("You need to provide a file")
       //   .test("type", "Only the following formats are accepted: .jpeg, .jpg, and .png", (value) => {
--- src/validations/eventValidationNew.js
+++ src/validations/eventValidationNew.js
@@ -7,7 +7,7 @@ class EventValidation {
       title: Yup.string().required("Required!").min(2).max(100),
       description: Yup.string().required("Required!").min(10).max(10000),
       startingDate : Yup.date().required("Required!"),
-      endingDate : Yup.date().required("Required!"),
+      endingDate : Yup.date().min(Yup.ref('startingDate'), 'Ending date must be greater than starting date').required("Required!"),
     });
   };
 }
